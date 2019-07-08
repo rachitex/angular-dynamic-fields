74f6fb28d6c6d986c65476bc88e343394f85d895
rachitex <rachitmishra5594@gmail.com>
2019-07-08 11:11:25 +0530

    Added feature to add the values in array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   form: FormGroup;
-  resourceQuantity: any;
+  // resourceQuantity: any;
 
   constructor() {
 
   }
   ngOnInit() {
     this.form = new FormGroup({
-            'employeeId': new FormControl(null, {validators: [Validators.required]}),
-            'employeeName': new FormControl(null, {validators: [Validators.required]}),
-            'resourceQuantity': new FormControl(null, {validators: [Validators.required]}),
-            'resourceId': new FormControl(null, {validators: [Validators.required]})
-        });
+      'employeeId': new FormControl(null, { validators: [Validators.required] }),
+      'employeeName': new FormControl(null, { validators: [Validators.required] }),
+      'resourceQuantity': new FormControl(null, { validators: [Validators.required] }),
+      'resources': new FormArray([])
+    });
+
+    this.resourceQuantity.valueChanges.subscribe(() => {
+      this.generateResourceForm();
+    });
   }
 
-  somethingChanged() {
-        console.log(this.resourceQuantity);
-    }
+  createResource() {
+    return new FormGroup({
+      'id': new FormControl(null, { validators: Validators.required})
+    });
+  }
 
-    counter(i: number) {
-        return new Array(i);
+  generateResourceForm() {
+    this.resources.clear();
+    for (let x = 0; x < this.resourceQuantity.value; x++) {
+      this.resources.push(this.createResource());
     }
+  }
+
+  add() {
+    console.log(this.form.value);
+  }
+
+  get resourceQuantity() {
+    return this.form.get('resourceQuantity');
+  }
+
+  get resources() {
+    return this.form.get('resources') as FormArray;
+  }
 }
